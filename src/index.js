e06c0535266b2132dcae38df9bfe6cb677718bbf
Daniel <danielmartin1444@gmail.com>
2023-01-15 18:31:31 +0000

    Added react-router-dom"

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 import App from './Containers/App/App';
 import './index.css';
@@ -8,7 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render
 (
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    (//)<React.StrictMode>
+      <App />
+    (//)</React.StrictMode>
+  </BrowserRouter>
 );
